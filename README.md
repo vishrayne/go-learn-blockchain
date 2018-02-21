9d3ed0754cfbb8a1b8f1cc70b2a4229432238553
vishnu ravi <vishnu.r@mobme.in>
2018-02-21 11:54:03 +0530

    adding cli support

--- README.md
+++ README.md
@@ -10,13 +10,54 @@ $ dep ensure
 ```
 2. `$ go build github.com/vishrayne/go-learn-blockchain/cmd/blockchain-cli`
 3. `$ go install github.com/vishrayne/go-learn-blockchain/cmd/blockchain-cli`
-4. `$ blockchain-cli`
+4. Run! 
+```
+$ blockchain-cli help
+
+    Usage:
+        addblock -data <BLOCK_DATA> - add the given block to the blockchain
+        printchain - print all the blocks of the chain
+
+$ blockchain-cli addblock -data "First transaction data"
+
+    Mining the block containing "Genesis block"
+    00000090785688fad3411423c11e084d03fbfba497d39f824a3adbb3dc2aaa95
+
+    Mining the block containing "First transaction data"
+    0000007056aa6db982634a0d4cea379a7741d06c3575472eb858a0e9df900855
+
+    Success!
+
+$ blockchain-cli addblock -data "second transaction data"
+
+    Mining the block containing "second transaction data"
+    00000071bce7b25ce51cb13c3f744050803acef28ffc6bbc76b1fb273042b86e
+
+    Success!
+
+$ blockchain-cli printchain
+
+    Prev block hash: 0000007056aa6db982634a0d4cea379a7741d06c3575472eb858a0e9df900855
+    Data: second transaction data
+    Hash: 00000071bce7b25ce51cb13c3f744050803acef28ffc6bbc76b1fb273042b86e
+    PoW: true
+
+    Prev block hash: 00000090785688fad3411423c11e084d03fbfba497d39f824a3adbb3dc2aaa95
+    Data: First transaction data
+    Hash: 0000007056aa6db982634a0d4cea379a7741d06c3575472eb858a0e9df900855
+    PoW: true
+
+    Prev block hash: 
+    Data: Genesis block
+    Hash: 00000090785688fad3411423c11e084d03fbfba497d39f824a3adbb3dc2aaa95
+    PoW: true
+```
 
 ## Progress/TODO
 - [x] Basic prototype
 - [x] Proof of work
 - [x] Persistence
-- [ ] CLI
+- [X] CLI
 - [ ] Transactions (Part I)
 - [ ] Addresses
 - [ ] Transactions (Part II)
--- cmd/blockchain-cli/main.go
+++ cmd/blockchain-cli/main.go
@@ -1,20 +1,93 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	blockchain "github.com/vishrayne/go-learn-blockchain"
 )
+
+// CLI will be our command line abstraction
+type CLI struct {
+	bc *blockchain.Blockchain
+}
 
 func main() {
 	bc := blockchain.NewBlockchain()
 	defer bc.Close()
 
-	bc.AddBlock("First transaction")
-	// bc.AddBlock("Second transaction")
-	// bc.AddBlock("Third transaction")
+	cli := CLI{bc}
+	cli.run()
+}
 
-	bci := bc.Iterator()
+func (cli *CLI) run() {
+	cli.validateArgs()
+
+	helpCmd := flag.NewFlagSet("help", flag.ExitOnError)
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("data", "", "<BLOCK_DATA>")
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	var err error
+	switch os.Args[1] {
+	case "help":
+		err = helpCmd.Parse(os.Args[2:])
+	case "addblock":
+		err = addBlockCmd.Parse(os.Args[2:])
+	case "printchain":
+		err = printChainCmd.Parse(os.Args[2:])
+	}
+
+	if err != nil {
+		fmt.Printf("Error occured while executing command => %v", err)
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if helpCmd.Parsed() {
+		cli.printUsage()
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
+
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("\taddblock -data <BLOCK_DATA> - add the given block to the blockchain")
+	fmt.Println("\tprintchain - print all the blocks of the chain")
+}
+
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
+	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	if len(data) <= 0 {
+		fmt.Println("Empty block data, skipping!")
+		os.Exit(1)
+	}
+
+	cli.bc.AddBlock(data)
+	fmt.Println("Success!")
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
 
 	for {
 		block := bci.Next()
